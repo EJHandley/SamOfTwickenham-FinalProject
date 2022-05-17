2ed1e897d11e45bea8b350c7225852327e537cee
Elliot <71550907+EJHandley@users.noreply.github.com>
2022-05-17 20:43:27 +0100

    Update 100522
    Created and Implemented new combat screen background
    Bones of narrative scene created
    Main Menu canvas created

---
+++ Assets/DialogueManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Response1()
+    {
+
+    }
+
+    void Response2()
+    {
+
+    }
+
+    void Response3()
+    {
+
+    }
+
+    void Response4()
+    {
+
+    }
+}
